c79f0acf05c5bcff8852aae1116cd26c58713950
Lex <nrazam95@gmail.com>
2022-02-12 17:43:01 +0800

    adding Random Int function

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ function capitalize(str) {
     return str.charAt(0) + str.slice(1).toLowerCase();
 }
 
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 module.exports = {
-    capitalize
+    capitalize,
+    getRandomInt,
 };
